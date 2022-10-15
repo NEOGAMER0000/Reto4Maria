3b88e95f6c39e7fd296fe377a96d81deabc2b76e
Your Name <you@example.com>
2022-10-15 13:32:42 -0500

    first commit

---
+++ src/main/java/com/example/demo/Repositorio/BikeRepositorio.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.Repositorio;
+
+import com.example.demo.Interface.BikeInterface;
+import com.example.demo.Modelo.Bike;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BikeRepositorio {
+    @Autowired
+    private BikeInterface bikeCrudRepository;
+    
+    public List<Bike> getAll(){
+        return (List<Bike>) bikeCrudRepository.findAll();
+    }
+    
+    public Optional<Bike> getBike(int id){
+        return bikeCrudRepository.findById(id);
+    }
+
+    public Bike save(Bike bike){
+        return bikeCrudRepository.save(bike);
+    }
+    
+    public void delete (Bike bike){
+        bikeCrudRepository.delete(bike);
+    }
+}
